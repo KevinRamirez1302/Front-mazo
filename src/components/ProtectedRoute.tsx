e903c2dfc3ba12e5f64b96cb7c29672e4d2fa3d6
Kevin Ramirez <kevin.alexanderra1302@gmail.com>
2026-04-15 13:38:22 +0200

    fixv4

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import type { ReactNode } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 
 export default function ProtectedRoute({ children }: { children: ReactNode }) {
